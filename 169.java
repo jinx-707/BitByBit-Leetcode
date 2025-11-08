4e6fd07b2e08b886f45946c04e674b8ee02e4ad8
Allu Saatvika Reddy <asvreddy77@gmail.com>
2025-11-08 13:25:45 +0530

    Create 169.java

---
+++ 169.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=0;
+        int num=0;
+        num=nums[0];
+        count=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==num){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count==0){
+                num=nums[i];
+                count=1;
+            }
+        }
+        return num;
+    }
+}
